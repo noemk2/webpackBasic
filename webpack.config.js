935ea6b7a9ece29f50ad78723befcfa88628002d
Noe Huaccharaque <noemk2@gmail.com>
2019-09-29 07:49:39 -0500

    sustitullendo las flags de webpack por instruccionese en webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//Ya no es nesesario especificar el archivo de origen y de salida con webpack
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    //path: 'build',
+    /*
+     *devemos darle una ruta absoluta, no una ruta relativa:una ruta absoluta es como /home/noe/Downloads/linux, mientras que una ruta realiva es un pedazo de ruta....Webpack es una herramienta de nodejs y usamos una constante de node llamada __dirname
+     */
+    path: __dirname + '/build',
+    filename: 'bundle.js',
+  },
+};
